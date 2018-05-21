9d6f1c0f88687133257cef4b49dd5b1d56f74256
Rodrigo Nunes <rodrigonunes.k2@gmail.com>
2018-05-21 20:17:43 -0300

    Inicio do projeto IMC. Aula 21-05-2018

---
+++ app/src/main/java/br/iff/edu/pooa20181/calculoimc/MainActivity.java
@@ -0,0 +1,13 @@
+package br.iff.edu.pooa20181.calculoimc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
